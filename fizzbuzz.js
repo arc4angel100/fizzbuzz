e1fff4a9fb2444511b4d00fd869d0a82e9af6890
Michael <minimike100@gmail.com>
2023-01-16 16:08:14 +0000

    revised for loop concantenate

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,13 @@
-for(i = 0; i <101; i++) {
-    if(i%3 === 0 && i%5 === 0) {
-        console.log("fizzbuzz")
-    } else if(i%3 === 0) {
-        console.log("fizz");
-    } else if (i%5 === 0) {
-        console.log("buzz");
-    } else {
+for(let i = 1; i <=105; i++) {
+    let message = "";
+    if(i%3 === 0)(message = "fizz")
+    if(i%5 === 0)(message += "buzz")
+    if(i%7 === 0)(message += "bang")
+    if(i%11 === 0)(message += "boom")
+
+    if (message === "") {
         console.log(i);
+    } else {
+        console.log(message)
     }
 }
